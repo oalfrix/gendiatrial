f0bcc3bea5c8856ed8ed6eccfb770e0a7f54d24f
alfrixo <140119318+oalfrix@users.noreply.github.com>
2023-10-18 18:41:52 +0300

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name']
+$visitor_email = $_POST['email']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+$email_from = 'github.com/iamtappy/gendiahighschool.com';
+
+$email_subject = 'New Form Submission'
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'tycoohamrk@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Loaction: contact.html");
+
+
+?>
+
